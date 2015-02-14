1c0fcbb91e1cff29e3de6b56906c23d0ed672625
samstephenson <sam.kayaker@hotmail.com>
2015-02-14 20:06:35 +0000

    Commented face zoom

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ $(document).ready(function(){
     // Target your .container, .wrapper, .post, etc.
     $("#mylightbox").fitVids();
 
-    $('img.headshot').click(function() {
+   /* $('img.headshot').click(function() {
     	$(this).toggleClass('zoomed');
     	$('.backgroundBlur').toggleClass('hidden');
-    });
+    });*/
 
 });
